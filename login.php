08dd99e35704485e4632584a5d5da992b5208184
Matheus Bosco <matheus98bosco@gmail.com>
2021-11-22 22:56:05 -0300

    2.0

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Ecommerce-Esportes</title>
+</head>
+<body>
+    <div>
+        <h1><strong>Login</strong></h1>
+        <form method="POST" action="processaLogin.php">
+            <label>Login:</label><input type="text" name="login" id="login"><br>
+            <label>Senha:</label><input type="password" name="senha" id="senha"><br>
+            <input type="submit" value="Entrar" id="Entrar" name="Entrar">
+            </form> 
+    </div>
+</body>
+</html>
---
+++ processaLogin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $hostname = "localhost";
+    $nome_bd = "ecomerce";
+
+    $user = $_POST['login'];
+    $password = $_POST['senha'];
+
+    try{
+        $bd = new PDO("mysql:dbname=".$nome_bd.";host=".$hostname, $user, $password);
+        echo "Bem-vindo ao Banco de Dados do Ecommerce de Esportes, <strong>".$user."</strong>!";
+        header('Location: principal.html');
+    }
+    catch(PDOException $e){
+        echo "Falha ao conectar: " . $e->getMessage();
+    }
+    catch(Exception $e){
+        echo "Erro genérico: " . $e->getMessage();
+    }
+    
+
+    ?>    
+</body>
+</html>
