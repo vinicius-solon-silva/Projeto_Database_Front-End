32d2b57d6414688009711ca9c0d64c0464fcaf6d
Vinicius_Silva <vinicius.solonsilva@hotmail.com>
2021-11-24 08:44:04 -0300

    Adicionado seção com formulários

--- processaLogin.php
+++ processaLogin.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,26 +12,36 @@
 
     $hostname = "localhost";
     $nome_bd = "00_ecommerce_esportes";
-
     $user = $_POST['login'];
     $password = $_POST['senha'];
 
     if($user == "admin"){
         try{
             $bd = new PDO("mysql:dbname=".$nome_bd.";host=".$hostname, $user, $password);
-            echo "Bem-vindo ao Banco de Dados do Ecommerce de Esportes, <strong>".$user."</strong>!<br>";
-            echo "Página do admin<br>";
+           
+            echo "<h2>Bem-vindo ao Banco de Dados do Ecommerce de Esportes, ".$user."!</h2>";
+            echo "<h3>Página do admin</h3><br>";
             
+            echo "<form>";
+            echo"<br> <section id='esquerda'><strong>CADASTRAR PRODUTO</strong>";
+            echo"<br> <label for='codigo'>Código do Produto</label> <input type='text' name='codigo'";
+            echo"<br><br> <label>Nome</label> <input type='text' name='nome'";
+            echo"<br><br> <label>Descrição</label> <input type='text' name='descricao'";
+            echo"<br><br> <label>Cor</label> <input type='text' name='cor'";
+            echo"<br><br> <label>Tamanho</label> <input type='text' name='tamanho'";
+            echo"<br><br> <label>Código da Categoria</label> <input type='text' name='categoria'";
+            echo"<br><br> <label>Preço</label> <input type='text' name='preco'";
+            echo"<br><br> <label>Código da Garantia</label> <input type='text' name='codigo' <br>";
+            echo"<br><input type='submit' value='cadastrar'></section>";
+            echo "</form>";
+
             //SELECT
-            $cmd = $bd->prepare("SELECT * FROM produto");
-            $cmd ->execute();
-            $resultado = $cmd->fetchAll(PDO::FETCH_ASSOC); //transforma em um array, e retorna somente as colunas
-            // echo "<pre>";
-            // print_r($resultado);
-            // echo "</pre>";
-            foreach ($resultado as $key => $value) {
-                echo $key.": ".$value."<br>";
-            }
+            // $cmd = $bd->prepare("SELECT * FROM produto");
+            // $cmd ->execute();
+            // $resultado = $cmd->fetchAll(PDO::FETCH_ASSOC); //transforma em um array, e retorna somente as colunas
+            // foreach ($resultado as $key => $value) {
+            //     echo $key.": ".$value."<br>";
+            // }
 
             //INSERT
             // $cmd = $bd->prepare("INSERT INTO garantia(Cod_gar, Descricao, Duracao, Valor) VALUES(:c, :de, :du, :v)");
@@ -53,8 +63,8 @@
     else if($user == "cliente"){
         try{
             $bd = new PDO("mysql:dbname=".$nome_bd.";host=".$hostname, $user, $password);
-            echo "Bem-vindo ao Banco de Dados do Ecommerce de Esportes, <strong>".$user."</strong>!<br>";
-            echo "Página do cliente";
+            echo "Bem-vindo ao Banco de Dados do Ecommerce de Esportes, <strong>".$user."</strong>!";
+            echo "<br>Página do cliente<br>";
         }
         catch(PDOException $e){
             echo "Falha ao conectar: " . $e->getMessage();
@@ -67,8 +77,9 @@
     else if($user == "transportadora"){
         try{
             $bd = new PDO("mysql:dbname=".$nome_bd.";host=".$hostname, $user, $password);
-            echo "Bem-vindo ao Banco de Dados do Ecommerce de Esportes, <strong>".$user."</strong>!<br>";
-            echo "Página da transportadora<br>";
+            echo "Bem-vindo ao Banco de Dados do Ecommerce de Esportes, <strong>".$user."</strong>!";
+            echo "<br>Página da transportadora<br>";
+            echo "<br>";
 
             $cmd = $bd->prepare("SELECT cli.Nome, cli.CEP, cli.No_casa, cli.Complemento, cli.Email, p.Nome AS Produto, c.Data_compra 
                                 FROM compra c 
@@ -78,10 +89,12 @@
                                 ON cli.CPF = c.CPF_cliente
                                 JOIN produto p  
                                 ON p.Cod_prod = car.Cod_prod 
-                                WHERE cli.CPF = 321234876 ");
+                                WHERE cli.CPF = 321234876");
             $cmd ->execute();
-            $resultado = $cmd->fetch();
-            print_r($resultado);
+            $resultado = $cmd->fetch(PDO::FETCH_ASSOC);
+            foreach ($resultado as $key => $value) {
+                echo $key.": ".$value."<br>";
+            }
 
         }
         catch(PDOException $e){
